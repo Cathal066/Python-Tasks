734e134baa47991b38538bca3a283b5abc8cd7c7
Cathal066 <ctw19.cathal.walsh@corketb.ie>
2024-09-11 12:16:20 +0100

    Add files via upload

---
+++ Refresher Task 2.py
@@ -0,0 +1,13 @@
+# Ask the user to input the number of hours they spend on a computer 
+hours = float(input("Enter the number of hours you spend on a computer on average per day: ")) # Check the input and print the corresponding message 
+if hours < 2: print("That seems reasonable.")
+elif hours < 4: print("Do you have time for anything else?")
+ else: print("You need to get some fresh air once in a while.")
+
+for _ in range(50): 
+ print("I am learning to code")
+
+# Ask the user to input the number of times they want the sentence to print count = int(input("Enter the number of times you want the sentence to print: ")) 
+ # Loop to print the sentence the specified number of times
+ for _ in range(count): 
+ print("I am learning to code")
